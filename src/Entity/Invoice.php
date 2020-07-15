15221e975191b1bf8dc8984361f999a922c899a8
lorem1291@gmail.com <44394308+Zulgsmh@users.noreply.github.com>
2020-07-15 15:29:09 +0200

    models/mise en place api_platform

--- src/Entity/Invoice.php
+++ src/Entity/Invoice.php
@@ -2,10 +2,21 @@
 
 namespace App\Entity;
 
-use App\Repository\InvoiceRepository;
 use Doctrine\ORM\Mapping as ORM;
+use App\Repository\InvoiceRepository;
+use ApiPlatform\Core\Annotation\ApiFilter;
+use ApiPlatform\Core\Annotation\ApiResource;
+use ApiPlatform\Core\Bridge\Doctrine\Orm\Filter\OrderFilter;
 
 /**
+ * @ApiResource(
+ * attributes={
+ *  "pagination_enabled"=true,
+ *  "pagination_items_per_page"=20,
+ *  "order":{"sentAt":"desc"}
+ * }
+ * )
+ * @ApiFilter(OrderFilter::class, properties={"amount","sentAt"})
  * @ORM\Entity(repositoryClass=InvoiceRepository::class)
  */
 class Invoice
